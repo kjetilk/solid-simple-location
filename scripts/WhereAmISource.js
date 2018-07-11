61e72336abf1392026472db1b078a582da37f480
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2018-07-12 01:16:26 +0200

    Hacklayer first braindump

--- scripts/WhereAmISource.js
+++ scripts/WhereAmISource.js
@@ -1,7 +1,25 @@
+// This was intended as a small script to just update my position
+// using the geolocation API. I set aside a couple of hours for it,
+// but when that had passed and I was still confused, I dropped the
+// ambition of making it work, rather writing a rough sketch of what
+// it should look like.
 
+// I would prefer if everything was initialised without global
+// variables, but I suppose I can live with this.
 var kb = $rdf.graph();
-var fetcher = new $rdf.Fetcher(kb); // store, timeout)
 
+const base_uri = 'https://solid.example.org/position/';
+
+// This is the agent actually doing requests against the remote server.
+const agent = new Agent(base_uri);
+
+// It should suffice to name the prefix, which should be looked up in
+// a curated set of mappings, or a crowdsources one if it doesn't
+// exist there.
+var ns = new Namespaces(['geo', 'rdf']);
+
+
+// This is cutnpaste from MDN Geolocation docs
 function geoFindMe() {
   var output = document.getElementById("out");
 
@@ -16,19 +34,36 @@ function geoFindMe() {
 
     output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
 
+
+    // We might want to display something like this, but not by sending our coordinates to someone else
     // var img = new Image();
     // img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
-
     // output.appendChild(img);
 
-    var body = '@prefix geo: <http://www.w3.org/2003/01/geo/wgs84_pos#> .\n\n<#posnow> a geo:Point ;\n\tgeo:long ' + longitude + ' ;\n\tgeo:lat ' + latitude +' .\n';
 
-    const response = await fetch('data/whereami.ttl', {
-      method: 'PUT',
-      headers: { 'Content-Type': 'text/turtle' },
-      body: body,
-	credentials: 'include'
+    // Now, we should build the RDF response. Here I think we should
+    // experiment with small trees to see if it can be made more
+    // intuitive for people not used to triples. This is just a
+    // brainstorm example, we should do a lot of them
+    kb.add({
+	about:'whereami#latestpos',
+	claim: [
+	    [ ns.a,           ns.geo('Point') ],
+	    [ ns.geo('lat'),  latitude ],
+	    [ ns.geo('long'), longitude ]
+	]
     });
+
+      
+    // There shouldn't be more to it than this. This should figure out
+    // the serialization and therefore the content type, that
+    // 'replace' means PUT in HTTP context, resolve relative to
+    // base_uri, make reasonable default actions with regards to
+    // authentication, etc
+    const response = agent.replace('whereami', kb);
+
+    // And then handle errors that agent.replace couldn't handle itself based on the response.
+      
   }
 
   function error() {
